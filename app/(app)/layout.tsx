7b7db6e351b996e469b9f09aea91e5ea0dd86e80
Alexander <alex@alexvoiceover.com>
2026-05-01 20:17:09 +0100

    fix: wrap (app) layout in Suspense for useSearchParams prerender

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,13 +1,16 @@
+import { Suspense } from "react";
 import { DroneParamsProvider } from "@/lib/drone-params-context";
 import { AppHeader } from "@/components/app-header";
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
-    <DroneParamsProvider>
-      <div className="flex h-screen flex-col overflow-hidden">
-        <AppHeader />
-        <main className="flex-1 overflow-y-auto min-h-0">{children}</main>
-      </div>
-    </DroneParamsProvider>
+    <Suspense>
+      <DroneParamsProvider>
+        <div className="flex h-screen flex-col overflow-hidden">
+          <AppHeader />
+          <main className="flex-1 overflow-y-auto min-h-0">{children}</main>
+        </div>
+      </DroneParamsProvider>
+    </Suspense>
   );
 }
